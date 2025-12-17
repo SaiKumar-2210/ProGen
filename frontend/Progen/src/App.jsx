69b790516eef4321dc5bbf5bd07e47111a120395
SaiKumar-2210 <saikumar22102005@gamil.com>
2025-12-17 22:16:17 +0530

    created tailwind env

--- frontend/Progen/src/App.jsx
+++ frontend/Progen/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import "tailwindcss";
 function App() {
 
   return (
-    <></>
+    <>
+    <div className="text-blue-3xl bg-blue w-5 p-20">OK OK</div>
+    Hello world!
+    </>
   )
 }
 
